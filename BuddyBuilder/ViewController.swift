b403e2d4b33d46e35da4790ab2141a4a96a6e85e
Ulrik Damm <ulrikdamm@me.com>
2015-02-06 12:49:32 +0100

    Initial Commit

---
+++ BuddyBuilder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BuddyBuilder
+//
+//  Created by Ulrik Damm on 06/02/15.
+//  Copyright (c) 2015 Robocat. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
